5feef47f6d73edf5ca2b10a37375fe7ce488f60e
Katherine Shakman <katherineshakman@hh10neurosc-001.cpmc.columbia.edu>
2017-09-07 18:12:17 -0400

    Tweaked batch script to use cellfun to prune the list of directories filenameList returned by dir call.

--- Batch_PI/Batch_PixelBasedPI.m
+++ Batch_PI/Batch_PixelBasedPI.m
@@ -6,7 +6,8 @@
 clear; close all; 
 
 %% Describe prefix to use to find movie files
-fileFinderToken = 'movie_*.ufmf'; 
+startToken = 'movie'; 
+extenToken = '.ufmf'; 
 %% Select folder in which to do batch processing on subfolders. 
 startDir = pwd; % So we can start and end in the same place.
 
@@ -20,7 +21,13 @@ dirNames = dirNames( cellfun(@(x) length(x)>4, dirNames) ); % Cleaned up list of
 for dirIdx = 1:length(dirNames)
     cd(dirNames{dirIdx}); % Go into the specified subfolder
     pathname = pwd; 
-    filenameList = dir(fileFinderToken); 
+    
+    filenameList = dir();
+    filenameList = filenameList(~[filenameList.isdir]); 
+    filenameList = {filenameList.name}'; 
+    filenameList = filenameList( cellfun(@(x) strcmp(x(1:5),'movie'), filenameList) ); % Get only the files that start with token
+    filenameList = filenameList( cellfun(@(x) strcmp(x(end-4:end),extenToken), filenameList) ); % Get only the files that end with token 
+    
     for fileIdx = 1:length(filenameList)
         close all; % Close all figures that were generated previously.
         filename = filenameList(fileIdx).name; 
